adb6f109a4846197746d40a312bd6e66f212c6aa
Scott Gale <galejscott@gmail.com>
2019-05-02 18:56:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,12 +10,13 @@ def speak_to_grandma(phrase)
 end
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  else 
   end
 end
 
